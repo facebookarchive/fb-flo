51044c59b5592b7e8691d1a90240afb73a8b6e9c
Amjad Masad <amjad.masad@gmail.com>
2014-02-22 22:31:53 -0500

    Add custom resolver test. Fix filepath. Add css test.

--- test/test.js
+++ test/test.js
@@ -52,6 +52,28 @@ describe('flo(dir)', function() {
     });
   });
 
+  it('should work with css', function(done) {
+    fs.writeFileSync('/tmp/flo_test/bar.css', 'bar {color: red}');
+    f = flo('/tmp/flo_test');
+    f.on('ready', function () {
+      client = new WebSocketClient();
+      client.connect('ws://localhost:8888/');
+      client.on('connectFailed', done.bind(null, new Error('Failed to connect')));
+      client.on('connect', function (connection) {
+        connection.on('message', function(msg) {
+          var data = msg.utf8Data;
+          var msg = JSON.parse(data);
+          assert.deepEqual(msg, {
+            contents: 'bar {color: blue}',
+            resourceURL: 'bar.css'
+          });
+          done();
+        });
+      });
+      fs.writeFileSync('/tmp/flo_test/bar.css', 'bar {color: blue}');
+    });
+  });
+
   it('should send resource to multiple clients when changed', function(done) {
     f = flo('/tmp/flo_test');
     f.on('ready', function () {
@@ -84,5 +106,49 @@ describe('flo(dir)', function() {
       fs.writeFileSync('/tmp/flo_test/foo.js', 'alert("hi")');
     });
   });
+});
 
+ describe('flo(dir, resolver)', function() {
+  var client, f;
+
+  beforeEach(function() {
+    try {
+      fs.mkdirSync('/tmp/flo_test');
+    } catch (e) {
+      // don't care
+    }
+
+    fs.writeFileSync('/tmp/flo_test/foo.js', 'alert("wow")');
+  });
+
+  afterEach(function() {
+    f.close();
+  });
+
+  it('should work with a custom resolver', function(done) {
+    f = flo('/tmp/flo_test', function (filepath, callback) {
+      assert.equal(filepath, 'foo.js');
+      callback({
+        contents: 'foobar',
+        resourceURL: 'customurl'
+      });
+    });
+
+    f.on('ready', function() {
+      client = new WebSocketClient();
+      client.connect('ws://localhost:8888/');
+      client.on('connect', function(connection) {
+        connection.on('message', function(msg) {
+          var data = msg.utf8Data;
+          var msg = JSON.parse(data);
+          assert.deepEqual(msg, {
+            contents: 'foobar',
+            resourceURL: 'customurl'
+          });
+          done();
+        });
+      });
+      fs.writeFileSync('/tmp/flo_test/foo.js', 'hmmmm');
+    });
+  });
 });
