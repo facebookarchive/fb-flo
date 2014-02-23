f79a30f916bd91cb26a05fdea440ebe78af4e01a
Amjad Masad <amjad.masad@gmail.com>
2014-02-22 23:17:42 -0500

    Implement match feature: * Every resolver callback instance can return a matching function to flo that would be sent to the client to be performed as a matching predicate to get the resource that would be updated. * the matching function could be a string indicating a built-in function: 	* "equal" which performs an equal check between on the resourceURL and resource in the browser. 	* "indexOf" preforms an index of and checks `> -1`. * it also could be a function or a regexp object that would be serialized and sent to the client to be recompiled and used as the match funciton.

--- test/test.js
+++ test/test.js
@@ -43,7 +43,8 @@ describe('flo(dir)', function() {
           var msg = JSON.parse(data);
           assert.deepEqual(msg, {
             contents: 'alert("hi")',
-            resourceURL: 'foo.js'
+            resourceURL: 'foo.js',
+            match: 'indexOf'
           });
           done();
         });
@@ -65,7 +66,8 @@ describe('flo(dir)', function() {
           var msg = JSON.parse(data);
           assert.deepEqual(msg, {
             contents: 'bar {color: blue}',
-            resourceURL: 'bar.css'
+            resourceURL: 'bar.css',
+            match: 'indexOf'
           });
           done();
         });
@@ -91,7 +93,8 @@ describe('flo(dir)', function() {
           var msg = JSON.parse(data);
           assert.deepEqual(msg, {
             contents: 'alert("hi")',
-            resourceURL: 'foo.js'
+            resourceURL: 'foo.js',
+            match: 'indexOf'
           });
           if (++i == 2) {
             done();
@@ -143,12 +146,82 @@ describe('flo(dir)', function() {
           var msg = JSON.parse(data);
           assert.deepEqual(msg, {
             contents: 'foobar',
-            resourceURL: 'customurl'
+            resourceURL: 'customurl',
+            match: 'indexOf'
           });
           done();
         });
       });
       fs.writeFileSync('/tmp/flo_test/foo.js', 'hmmmm');
+    });
+  });
+
+  describe('resolver match property', function() {
+    it('should work with a custom resolver', function(done) {
+      f = flo('/tmp/flo_test', function (filepath, callback) {
+        assert.equal(filepath, 'foo.js');
+        callback({
+          contents: 'foobar',
+          resourceURL: 'customurl',
+          match: function (a, b) {return a == b;}
+        });
+      });
+
+      f.on('ready', function() {
+        client = new WebSocketClient();
+        client.connect('ws://localhost:8888/');
+        client.on('connect', function(connection) {
+          connection.on('message', function(msg) {
+            var data = msg.utf8Data;
+            var msg = JSON.parse(data);
+            assert.deepEqual(msg, {
+              contents: 'foobar',
+              resourceURL: 'customurl',
+              match: {
+                type: 'function',
+                source: 'function (a, b) {return a == b;}'
+              }
+            });
+            done();
+          });
+        });
+        fs.writeFileSync('/tmp/flo_test/foo.js', 'hmmmm');
+      });
+    });
+
+    it('should work with a custom resolver', function(done) {
+      f = flo('/tmp/flo_test', function (filepath, callback) {
+        assert.equal(filepath, 'foo.js');
+        callback({
+          contents: 'foobar',
+          resourceURL: 'customurl',
+          match: /a(b)+/gi
+        });
+      });
+
+      f.on('ready', function() {
+        client = new WebSocketClient();
+        client.connect('ws://localhost:8888/');
+        client.on('connect', function(connection) {
+          connection.on('message', function(msg) {
+            var data = msg.utf8Data;
+            var msg = JSON.parse(data);
+            assert.deepEqual(msg, {
+              contents: 'foobar',
+              resourceURL: 'customurl',
+              match: {
+                type: 'regexp',
+                global: true,
+                ignoreCase: true,
+                multiline: false,
+                source: 'a(b)+'
+              }
+            });
+            done();
+          });
+        });
+        fs.writeFileSync('/tmp/flo_test/foo.js', 'hmmmm');
+      });
     });
   });
 });
