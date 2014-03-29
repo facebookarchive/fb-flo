c9501ebb3ab35eca1a75de7eb541b985023cd7d5
Amjad Masad <amjad.masad@gmail.com>
2014-03-29 12:24:09 -0400

    Documentation & cleanup

--- client/index.js
+++ client/index.js
@@ -4,8 +4,15 @@
 (function() {
   'use strict';
 
+  /**
+   * Flo client controller.
+   *
+   * @class FloClient
+   * @private
+   */
+
   function FloClient() {
-    this.loadConfig();
+    this.config = loadConfig();
     this.session = null;
     this.panelWindow = null;
     this.panelEventBuffer = [];
@@ -17,31 +24,12 @@
     this.start();
   }
 
-  FloClient.prototype.loadConfig = function() {
-    var config;
-    try {
-      config = JSON.parse(localStorage.getItem('flo-config'));
-    } catch (e) {
-      // Ignore parse error.
-    }
-    if (config) {
-      config.hostnames = config.hostnames.map(function(pattern) {
-        var m = pattern.match(/^\/(.+)\/([gim]{0,3})$/);
-        if (m && m[1]) {
-          return new RegExp(m[1], m[2]);
-        } else {
-          return pattern;
-        }
-      });
-    } else {
-      config = {
-        hostnames: [],
-        port: 8888
-      };
-    }
-
-    this.config =  config;
-  };
+  /**
+   * Save and optionally set new config.
+   *
+   * @param {object} config
+   * @private
+   */
 
   FloClient.prototype.saveConfig = function(config) {
     if (config) {
@@ -50,6 +38,15 @@
     localStorage.setItem('flo-config', JSON.stringify(this.config));
   };
 
+  /**
+   * Listen on the panel window for an event `type`, i.e. receive a message
+   * from the panel.
+   *
+   * @param {string} type
+   * @param {function} callback
+   * @private
+   */
+
   FloClient.prototype.listenToPanel = function(type, callback) {
     if (!this.panelWindow) {
       throw new Error('Panel not found');
@@ -57,7 +54,16 @@
     this.panelWindow.addEventListener('flo_' + type, callback.bind(this));
   };
 
-  FloClient.prototype.triggerEvent = function (type, data) {
+  /**
+   * Trigger an event on the panel window, i.e. send a message to the panel.
+   * If the panel wasn't instantiated yet, the event is buffered.
+   *
+   * @param {string} type
+   * @param {object} data
+   * @private
+   */
+
+  FloClient.prototype.triggerEvent = function(type, data) {
     var event = new Event('flo_' + type);
     event.data = data;
     if (this.panelWindow) {
@@ -67,6 +73,13 @@
     }
     return event;
   };
+
+  /**
+   * Create a new panel.
+   *
+   * @param {function} callback
+   * @private
+   */
 
   FloClient.prototype.createPanel = function(callback) {
     var self = this;
@@ -83,6 +96,14 @@
     );
   };
 
+  /**
+   * Called after the panel is first created to listen on it's events.
+   * Will also trigger all buffered events on the panel.
+   *
+   * @param {object} config
+   * @private
+   */
+
   FloClient.prototype.bindPanelEvents = function() {
     this.listenToPanel('config_changed', function(e) {
       this.saveConfig(e.data);
@@ -97,16 +118,35 @@
     this.triggerEvent('load', this.config);
   };
 
+  /**
+   * Starts the flo client.
+   *
+   * @private
+   */
+
   FloClient.prototype.start = function() {
     this.status('starting');
     this.startNewSession();
   };
 
 
+  /**
+   * Stops flo client.
+   *
+   * @private
+   */
+
   FloClient.prototype.stop = function() {
     this.session.destroy();
     this.session = null;
   };
+
+  /**
+   * Get the url location of the inspected window.
+   *
+   * @param {function} callback
+   * @private
+   */
 
   FloClient.prototype.getLocation = function(callback) {
     chrome.devtools.inspectedWindow['eval'](
@@ -114,6 +154,13 @@
       callback.bind(this)
     );
   };
+
+  /**
+   * Match config patterns against `host`.
+   *
+   * @param {string} host
+   * @private
+   */
 
   FloClient.prototype.matchHost = function(host) {
     var config = this.config;
@@ -129,6 +176,12 @@
     }
     return false;
   };
+
+  /**
+   * Instantiates a new `session`.
+   *
+   * @private
+   */
 
   FloClient.prototype.startNewSession = function() {
     if (this.session) {
@@ -152,6 +205,12 @@
     );
   };
 
+  /**
+   * Enables flo for the current inspected window host.
+   *
+   * @private
+   */
+
   FloClient.prototype.enableForHost = function() {
     this.getLocation(function(host) {
       if (!this.matchHost(host)) {
@@ -162,6 +221,14 @@
       }
     });
   };
+
+  /**
+   * Reports status changes to panel.
+   *
+   * @param {string} status
+   * @param {object} aux
+   * @private
+   */
 
   FloClient.prototype.status = function(status, aux) {
     var text, action;
@@ -200,5 +267,39 @@
     });
   };
 
+  /**
+   * Loads config from localstorage.
+   *
+   * @private
+   */
+
+  function loadConfig() {
+    var config;
+    try {
+      config = JSON.parse(localStorage.getItem('flo-config'));
+    } catch (e) {
+      // Ignore parse error.
+    }
+    if (config) {
+      config.hostnames = config.hostnames.map(function(pattern) {
+        var m = pattern.match(/^\/(.+)\/([gim]{0,3})$/);
+        if (m && m[1]) {
+          return new RegExp(m[1], m[2]);
+        } else {
+          return pattern;
+        }
+      });
+    } else {
+      config = {
+        hostnames: [],
+        port: 8888
+      };
+    }
+
+    return config;
+  }
+
+  // Start the app.
   new FloClient();
+
 })();
